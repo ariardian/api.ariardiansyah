dbf600dc2da31bd757c68e3f4fabbde9944fb1b7
Ari Ardiansyah <ary.ardyan@gmail.com>
2020-10-14 23:11:38 +0700

    add FE

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const path = require("path");
 
 // parse requests of content-type - application/json (parse req)
 app.use(bodyParser.json());
@@ -10,15 +11,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Ari Ardiansyah application for API." });
-});
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to Ari Ardiansyah application for API." });
+// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 require("./src/routes/customer.routes.js")(app);
 require("./src/routes/authRoutes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
