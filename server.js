72379d41eb77a5e802be4f748e88d7914d08315f
Ari Ardiansyah <ary.ardyan@gmail.com>
2020-10-15 12:28:28 +0700

    edit server js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to Ari Ardiansyah application for API." });
-// });
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Ari Ardiansyah application for API." });
+});
+// if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 require("./src/routes/customer.routes.js")(app);
 require("./src/routes/authRoutes.js")(app);
